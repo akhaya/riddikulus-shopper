319006c2a718b0d91634fe92fb48cdaee50dcde8
Jin Wang <jin.wang.527@gmail.com>
2017-03-01 15:28:31 -0500

    add productsreducer and produtscontainer

--- app/main.jsx
+++ app/main.jsx
@@ -3,10 +3,21 @@ import React from 'react'
 import {Router, Route, IndexRedirect, IndexRoute, browserHistory} from 'react-router'
 import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
-
 import store from './store'
+import axios from 'axios';
+
 import NavbarComponent from './components/NavbarComponent'
-import Products from './components/Products'
+import ProductsContainer from './containers/ProductsContainer'
+import {receiveProducts} from './reducers/products'
+
+const onAppEnter = () => {
+  axios.get('/api/products')
+  .then(res => {
+    console.log('=====', res.data)
+    store.dispatch(receiveProducts(res.data))
+  })
+  .catch(console.error.bind(console))
+}
 
 const App = connect(
   ({ auth }) => ({ user: auth })
@@ -21,8 +32,9 @@ const App = connect(
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Products} />
+      <Route path="/" component={App} onEnter={onAppEnter}>
+        <IndexRoute component={ProductsContainer} />
+        <Route path="/products" component={ProductsContainer} />
       </Route>
         {/*<IndexRedirect to="/jokes" />
         <Route path="/jokes" component={Jokes} />
