0dbe2520951f755f446154eda4f07c700c18e643
Christine <cleverett1@gmail.com>
2017-03-01 11:32:05 -0500

    removed getterMethod and added product/orderline association

--- db/models/order.js
+++ db/models/order.js
@@ -1,17 +1,40 @@
 const Sequelize = require('sequelize')
 const db = require('APP/db')
+const Orderline = require('./orderline')
 
 
 const Order = db.define('orders', {
-  orderDate: {
-    // is this necessary if we have a created_at we can use??
-    type: Sequelize.DATE,
-  },
   status:{
-    // only admin should be able to change the status
+    // only admin should be able to change the status -- make sure to check before updating
     type: Sequelize.ENUM,
-    values: ['processing', 'shipped', 'delivered']
+    values: ['pending', 'processing', 'shipped', 'delivered'],
+    defaultValue: 'pending',
   },
+
+  shippingAddress:{
+    // can be null if user is a guest user and is still browsing,
+    // should not be null once they want to place order
+    type: Sequelize.STRING,
+  },
+
+  shippingCost:{
+    type: Sequelize.INTEGER,
+  },
+
+  tax: {
+    type: Sequelize.INTEGER,
+  },
+
+  subtotal: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+  },
+
+  totalCost: {
+    type: Sequelize.INTEGER,
+    defaultValue:0,
+  }
+
 })
 
 module.exports = Order
--- db/models/order.test.js
+++ db/models/order.test.js
@@ -2,19 +2,28 @@
 
 const db = require('APP/db')
 const Order = require('./order')
+const Orderline = require('./orderline')
 const User = require('./user')
 const {expect} = require('chai')
 
-describe('Order', () => {
+describe('Order Model', () => {
   before('wait for the db', () => db.didSync)
 
+    var order1;
 
+    beforeEach('create an order with some orderlines', () => {
+      order1 = Order.create({
+        status: 'pending'
+      })
+      // let order2 = Order.create({
+      //   status: 'pending'
+      // })
 
-    describe('Order Model', () => {
+      // return Promise.all(order1, order2);
+    })
 
       // This assertion expects that the Order model will put a `status` column in the messages table.
       it('has status as part of definition', () => {
-
         expect(Order.attributes.status).to.be.an('object');
       })
 
@@ -30,10 +39,8 @@ describe('Order', () => {
           })
         }).then(newOrder => {
             expect(newOrder.user_id).to.equal(userId)
-          })
+        })
 
       })
 
-
-    });
 })
