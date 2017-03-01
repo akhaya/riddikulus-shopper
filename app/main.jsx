e5572a2bee286534c8cedb287d222971e865207c
Christine <cleverett1@gmail.com>
2017-03-01 16:45:32 -0500

    route added

--- app/main.jsx
+++ app/main.jsx
@@ -1,6 +1,6 @@
 'use strict'
 import React from 'react'
-import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
+import {Router, Route, IndexRedirect, browserHistory, IndexRoute} from 'react-router'
 import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 
@@ -9,6 +9,8 @@ import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 
+import SingleProduct from './components/SingleProduct'
+
 const ExampleApp = connect(
   ({ auth }) => ({ user: auth })
 ) (
@@ -16,7 +18,7 @@ const ExampleApp = connect(
     <div>
       <nav>
         {user ? <WhoAmI/> : <Login/>}
-      </nav> 
+      </nav>
       {children}
     </div>
 )
@@ -25,8 +27,7 @@ render (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={ExampleApp}>
-        <IndexRedirect to="/jokes" />
-        <Route path="/jokes" component={Jokes} />
+        <IndexRoute component={SingleProduct} />
       </Route>
     </Router>
   </Provider>,
