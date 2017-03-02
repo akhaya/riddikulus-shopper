77d011db769f71237e9122723be08b9203cf7b93
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-03-02 18:27:11 -0500

    finish basic view for filter

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import {Link} from 'react-router'
 
 export default (props) => {
-  // console.log('=====', props)
   const products = props.products
+
+  if (!products) {
+    return null
+  }
+  const magicalAbilities = products.reduce((accumulator, current) => {
+    return accumulator.concat(current.magicalAbilities)
+  }, [])
+
+  const sizes = ['XS', 'S', 'M', 'L', 'XL']
 
   // LOOK INTO LODASH METHODS. WHEN WE MAP THROUGH PRODUCT.BREED, MAY HAVE MULTIPLE BUT ONLY WANT TO LIST THE UNIQUE BREEDS.
 
-  console.log(products)
-
+  // LIFESPAN AND PRICE SLIDERS DON'T SHOW THE NUMBER VALUE YET
   return (
 
     <div className="container-fluid">
@@ -16,26 +23,44 @@ export default (props) => {
 
         <div className="col-xs-2">
           <h2> Breeds </h2>
-              {products && products.map((product, id) => {
+              {products.map((product, id) => {
                     return (
-                    <div>
+                    <div key={id}>
                       <label className="form-check-label">
-                        <input key={id} className="form-check-input" type="checkbox" value="" />{product.breed.name}
+                        <input className="form-check-input" type="checkbox" value="" />{product.breed.name}
                       </label>
                     </div>)
                 })}
           <h2> Filters </h2>
           <h3> Magical Abilities </h3>
-          <h3> size </h3>
-          <h3> lifespan </h3>
+              {magicalAbilities.map((magicalAbility, id) => {
+                    return (
+                    <div key={id}>
+                      <label className="form-check-label">
+                        <input className="form-check-input" type="checkbox" value="" />{magicalAbility}
+                      </label>
+                    </div>)
+                })}
+          <h3> Size </h3>
+              {sizes.map((size, id) => {
+                    return (
+                    <div key={id}>
+                      <label className="form-check-label">
+                        <input className="form-check-input" type="checkbox" value="" />{size}
+                      </label>
+                    </div>)
+                })}
+          <h3> Lifespan </h3>
+            <input type="range" min="0" max="100" />
           <h3> Price </h3>
+            <input type="range" min="0" max="5000" />
         </div>
 
         <div className="col-xs-10">
           <h3>Creatures</h3>
           <div className="row">
 
-            {products && products.map(product => {
+            {products.map(product => {
             return (<div className="col-xs-3" key = {product.id}>
               {/* add onclick handler to single product view */}
               <a className="thumbnail" href="#" onClick={() => clickedAlbum(album.id)}>
