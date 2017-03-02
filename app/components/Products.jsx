c70ffc5e9051f38af465ef5c5b06ee6c9bf7c57c
Christine <cleverett1@gmail.com>
2017-03-02 17:51:34 -0500

    filter by breed

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import {Link} from 'react-router'
 
 export default (props) => {
-  console.log('=====', props)
+  // console.log('=====', props)
   const products = props.products
+
+  // LOOK INTO LODASH METHODS. WHEN WE MAP THROUGH PRODUCT.BREED, MAY HAVE MULTIPLE BUT ONLY WANT TO LIST THE UNIQUE BREEDS.
 
   console.log(products)
 
@@ -14,9 +16,14 @@ export default (props) => {
 
         <div className="col-xs-2">
           <h2> Breeds </h2>
-          {
-
-          }
+              {products && products.map((product, id) => {
+                    return (
+                    <div>
+                      <label className="form-check-label">
+                        <input key={id} className="form-check-input" type="checkbox" value="" />{product.breed.name}
+                      </label>
+                    </div>)
+                })}
           <h2> Filters </h2>
           <h3> Magical Abilities </h3>
           <h3> size </h3>
