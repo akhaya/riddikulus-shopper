57ac7225c21442e94429fbc377ebeeb976fcbb4f
Jin Wang <jin.wang.527@gmail.com>
2017-03-04 01:24:32 -0500

    user can delete orderline

--- app/containers/CartContainer.jsx
+++ app/containers/CartContainer.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import OrderItem from '../components/OrderItem'
 import CartSidebar from '../components/CartSidebar'
-
+import {deleteOrderItemFromUserCart} from '../reducers/cart'
 
 class CartContainer extends Component {
   constructor(props){
@@ -16,7 +16,7 @@ class CartContainer extends Component {
 
   calculateSubtotal(){
     const orderlines = this.props.cart.orderlines
-    if(orderlines){
+    if(orderlines && orderlines.length > 0){
       return orderlines.map(ol => ol.subtotal).reduce( (a,b) => a+b )
     }
     return 0
@@ -38,19 +38,20 @@ class CartContainer extends Component {
   render(){
     const cart = this.props.cart
     const orderlines = cart.orderlines
+    const handleDelete = this.props.handleDelete
+    const userId = cart.user_id
     const noItemsMessage = (
     <div className="panel panel-default">
       <div className="panel-body">
         <h4>  You have no items in your cart. </h4>
       </div>
     </div>)
-
     return (
       <div className="container">
         <h3>Cart</h3>
         <div className="row">
           <div className="col-md-9">
-            {orderlines? orderlines.map(orderline => <OrderItem orderline={orderline} key={orderline.id} />) : noItemsMessage}
+            {orderlines && orderlines.length > 0 ? orderlines.map(orderline => <OrderItem orderline={orderline} handleDelete={handleDelete} userId={userId} key={orderline.id} />) : noItemsMessage}
           </div>
           <div className="col-md-3">
             <CartSidebar orderTotals={{
@@ -72,5 +73,13 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(CartContainer)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleDelete (userId, orderId, productId) {
+      dispatch(deleteOrderItemFromUserCart(userId, orderId, productId))
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartContainer)
 
