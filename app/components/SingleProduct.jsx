60fb9c36438e9a60c7ceb773cc8908ac38289351
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-03-05 16:16:13 -0500

    add form event listeners on single product form

--- app/components/SingleProduct.jsx
+++ app/components/SingleProduct.jsx
@@ -1,84 +1,123 @@
-import React from 'react'
+import React, {Component} from 'react'
 import _ from 'lodash'
 import {convertPrice} from '../utils'
 
-export default (props) => {
-  const animal = props.product
+class SingleProduct extends Component {
+  constructor(props) {
+    super(props)
 
-  if (!animal) return null
+    this.state = {
+      color: '',
+      quantity: 1
+    }
 
-  const quantity = _.range(0, animal.inventory);
+    this.onColorChange = this.onColorChange.bind(this)
+    this.onQuantityChange = this.onQuantityChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-  return(
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-sm-6 thumbnail">
-          <img src={animal.pictureURL} />
-        </div>
-        <div className="col-sm-4">
-          <div>
-            <h1> Name: {animal.name} </h1>
-            <h1> Price: </h1>
-            <h4>  {animal.price}  </h4>
-            <h1> Profile:</h1>
-            <h4> {animal.description} </h4>
-            <h4> Colors </h4>
-            <select>
-              {
-                animal.colors.map((color, id) => {
-                  return <option key={id} value={color}> {color}</option>
-                })
-              }
-            </select>
-            <h4> Quantity </h4>
-            {
-              <select>
+  onColorChange (event) {
+    event.preventDefault()
+    this.setState({color: event.target.value})
+  }
+
+  onQuantityChange (event) {
+    event.preventDefault()
+    this.setState({quantity: event.target.value})
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    // user must choose color before adding to cart
+    // quantity defaults to 1 if user doesn't select quantity
+    if (this.state.color) {
+      console.log('====', this.state.color, this.state.quantity)
+    }
+  }
+
+  render() {
+    const animal = this.props.product
+
+    if (!animal) return null
+
+    const quantity = _.range(1, animal.inventory);
+
+    return(
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6 thumbnail">
+            <img src={animal.pictureURL} />
+          </div>
+          <div className="col-sm-4">
+            <div>
+              <h1> Name: {animal.name} </h1>
+              <h1> Price: </h1>
+              <h4>  {animal.price}  </h4>
+              <h1> Profile:</h1>
+              <h4> {animal.description} </h4>
+              <h4> Colors </h4>
+              <select onChange={this.onColorChange}>
+                {/* added a blank option because didn't know how to automatically set local state to the this.props.product.color[0] */}
+                <option> </option>
                 {
-                  quantity.map(num => {
-                    return <option key={num} value={num}> {num}</option>
+                  animal.colors.map((color, id) => {
+                    return <option key={id} value={color}> {color}</option>
                   })
                 }
               </select>
-            }
-            <div>
-              <button type="button" className="btn btn-default" aria-label="Left Align">
-                <div className="glyphicon glyphicon-shopping-cart" aria-hidden="true"></div>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className='row'>
-        <div className="col-sm-8">
-          <h1> Reviews </h1>
-            <div className="panel panel-default">
-              <div className="panel-body">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <h4> Quantity </h4>
+              {
+                <select onChange={this.onQuantityChange}>
+                  {
+                    quantity.map(num => {
+                      return <option key={num} value={num}> {num}</option>
+                    })
+                  }
+                </select>
+              }
+              <div>
+                <button type="button" className="btn btn-default" aria-label="Left Align" onClick={this.handleSubmit}>
+                  <div className="glyphicon glyphicon-shopping-cart" aria-hidden="true"></div>
+                </button>
               </div>
             </div>
           </div>
-        <div className="col-sm-4">
-          <h1> User Photos </h1>
-            <div className="col-sm-5">
-              <img className='thumbnailImg' src={animal && animal.pictureURL} />
+        </div>
+        <div className='row'>
+          <div className="col-sm-8">
+            <h1> Reviews </h1>
+              <div className="panel panel-default">
+                <div className="panel-body">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+              </div>
             </div>
-            <div className="col-sm-5">
-              <img className='thumbnailImg' src={animal && animal.pictureURL} />
-            </div>
-            <div className="col-sm-5">
-              <img className='thumbnailImg' src={animal && animal.pictureURL} />
-            </div>
-            <div className="col-sm-5">
-              <img className='thumbnailImg' src={animal && animal.pictureURL} />
-            </div>
+          <div className="col-sm-4">
+            <h1> User Photos </h1>
+              <div className="col-sm-5">
+                <img className='thumbnailImg' src={animal && animal.pictureURL} />
+              </div>
+              <div className="col-sm-5">
+                <img className='thumbnailImg' src={animal && animal.pictureURL} />
+              </div>
+              <div className="col-sm-5">
+                <img className='thumbnailImg' src={animal && animal.pictureURL} />
+              </div>
+              <div className="col-sm-5">
+                <img className='thumbnailImg' src={animal && animal.pictureURL} />
+              </div>
+          </div>
         </div>
       </div>
-    </div>
 
-  )
+    )
+
+  }
 }
+
+export default SingleProduct
