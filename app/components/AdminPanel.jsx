47a0e83a29acdb2160f1fa89b707ba96601eb946
Aly Khayrulina <a.a.khayrulina@gmail.com>
2017-03-06 12:33:35 -0500

    closes issue 65

--- app/components/AdminPanel.jsx
+++ app/components/AdminPanel.jsx
@@ -5,16 +5,16 @@ export default (props) => {
   return (
       <div className="container-fluid">
         <h3>Admin Panel</h3>
-      <div className="container-fluid" role="navigation">
-        <ul className="nav nav-tabs">
-          <li role="presentation" className="active"><Link to="/admin/users">Users</Link></li>
-          <li role="presentation"><Link to="/admin/orders">Orders</Link></li>
-          <li role="presentation"><Link to="/admin/products">Products</Link></li>
-        </ul>
-      </div>
-      <div className="container-fluid">
-        {props.children}
-      </div>
+        <div className="container-fluid" role="navigation">
+          <ul className="nav nav-tabs">
+            <li role="presentation" className="active"><Link to="/admin/users">Users</Link></li>
+            <li role="presentation"><Link to="/admin/orders">Orders</Link></li>
+            <li role="presentation"><Link to="/admin/products">Products</Link></li>
+          </ul>
+        </div>
+        <div className="container-fluid">
+          {props.children}
+        </div>
     </div>
   )
 }
