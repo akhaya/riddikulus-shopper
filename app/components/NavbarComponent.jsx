e166da84066cd8d6307d0bfb21c50c705a976b51
Christine <cleverett1@gmail.com>
2017-03-05 21:56:01 -0500

    added font

--- app/components/NavbarComponent.jsx
+++ app/components/NavbarComponent.jsx
@@ -10,18 +10,13 @@ export default (props) => {
   return (
     <div>
       <nav className="navbar navbar-default">
-        <div className="container-fluid">
+        <div className="container-fluid" id="mainNav">
           <div className="navbar-header">
-           <Link to={'/products'} className="navbar-brand">Riddikulus</Link>
+           <Link to={'/products'} className="navbar-brand" id="shopperName">Riddikulus</Link>
           </div>
         <ul className="nav navbar-nav navbar-right">
-          <li>
+          <li className="nav-login">
             {props.user ? <WhoAmI/> : <Login/>}
-          </li>
-           <li>
-            <Link to={'/signup'}>
-              <button type="button" className="btn btn-danger">Sign up!</button>
-            </Link>
           </li>
         </ul>
         </div>
