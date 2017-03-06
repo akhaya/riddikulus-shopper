c775536cdbb5ac79b9b999d790090577829700a9
Christine <cleverett1@gmail.com>
2017-03-06 15:13:16 -0500

    breeds and size filters working

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -7,23 +7,54 @@ class Products extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      size: ''
+      size: [],
+      breeds: [],
+      magicalAbilities: [],
+      lifespan: 1000,
+      price: 20000
     }
     this.handleSizeInput = this.handleSizeInput.bind(this)
+    this.handleBreedInput = this.handleBreedInput.bind(this)
+    this.handleMagicalAbilitiesInput = this.handleMagicalAbilitiesInput.bind(this)
   }
 
   handleSizeInput (event) {
     // event.preventDefault()
-    console.log('====', event.target.checked)
     if (event.target.checked) {
-      this.setState({size: event.target.value})
+      this.setState({size: this.state.size.concat([event.target.value])})
     } else {
-      this.setState({size: ''})
+      let i = this.state.size.indexOf(event.target.value)
+      let x = this.state.size.splice(i,1)
+      this.setState({size: this.state.size})
+    }
+  }
+
+  handleBreedInput(event){
+    if(event.target.checked){
+      this.setState({breeds: this.state.breeds.concat([event.target.value])})
+    }
+    else{
+      let i = this.state.breeds.indexOf(event.target.value)
+      let x = this.state.breeds.splice(i,1)
+      this.setState({breeds: this.state.breeds})
+    }
+  }
+
+  handleMagicalAbilitiesInput(event){
+    if(event.target.checked){
+      this.setState({magicalAbilities: this.state.magicalAbilities.concat([event.target.value])})
+    }
+    else{
+      let i = this.state.magicalAbilities.indexOf(event.target.value)
+      let x = this.state.magicalAbilities.splice(i, 1)
+      this.setState({magicalAbilities: this.state.magicalAbilities})
     }
   }
 
   render() {
+    console.log(this.state.breeds)
     const products = this.props.products
+    console.log("********* products", products)
 
     if (!products) {
       return null
@@ -33,13 +64,11 @@ class Products extends Component {
     }, []));
 
 
-    // let breeds = _.uniq(products.breeds);
-
-    // let breeds = _.uniq(products.map(product => {
-    //   return product.breed
-    // }).map(breed => {
-    //   return breed.name
-    // }))
+    let breeds = _.uniq(products.map(product => {
+      return product.breed
+    }).map(breed => {
+      return breed.name
+    }))
 
     const sizes = ['XS', 'S', 'M', 'L', 'XL']
 
@@ -49,24 +78,24 @@ class Products extends Component {
         <div className="row">
 
           <div className="col-xs-2">
-          {/*}
+
             <h3> Breeds </h3>
                 {breeds.map((breed, id) => {
                       return (
                       <div key={id}>
                         <label className="form-check-label">
-                          <input className="form-check-input" type="checkbox" onChange={handleChange} />{breed}
+                          <input className="form-check-input" type="checkbox" value={breed} onClick={this.handleBreedInput}/>{breed}
                         </label>
                       </div>)
                   })}
-            */}
+
             <h3> Filters </h3>
             <h4> Magical Abilities </h4>
                 {magicalAbilities.map((magicalAbility, id) => {
                       return (
                       <div key={id}>
                         <label className="form-check-label">
-                          <input className="form-check-input" type="checkbox" value={magicalAbility} />{magicalAbility}
+                          <input className="form-check-input" type="checkbox" onClick={this.handleMagicalAbilitiesInput} value={magicalAbility} />{magicalAbility}
                         </label>
                       </div>)
                   })}
@@ -89,7 +118,7 @@ class Products extends Component {
 
               {products
                 .filter(product => {
-                  return this.state.size ? product.size === this.state.size : product
+                  return this.state.breeds.length > 0 ? this.state.breeds.indexOf(product.breed.name) > -1 : product
                 })
                 .map(product => {
                   return (<div className="col-xs-3" key={product.id}>
@@ -97,6 +126,7 @@ class Products extends Component {
                     <a className="thumbnail">
                       <img src={product.pictureURL} />
                       <div className="caption">
+                        <h5> {product.name} </h5>
                         <h5>
                           <span>{product.price} Galleons</span>
                         </h5>
