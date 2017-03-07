0da8c2014fba8adc52ffd24d4d04170df84982e0
Jin Wang <jin.wang.527@gmail.com>
2017-03-07 17:02:36 -0500

    update address model getter method

--- app/components/Address.jsx
+++ app/components/Address.jsx
@@ -4,17 +4,10 @@ import React from 'react'
 export default (props) => {
   console.log('======checkout', props)
   return (
-    <div className="row">
-    <div className="col-md-1">
-    </div>
-      <div className="col-md-8">
-        <div className="panel panel-default">
-          <div className="panel-body">
-            Address
-          </div>
-        </div>
-        <button className="btn btn-default" type="submit">Place Order</button>
-        <hr />
+    <div className="container">
+      <div className="row">
+        <h2>Address</h2>
+      {/* add address from props - need to eager load address into order */}
       </div>
     </div>
   )
